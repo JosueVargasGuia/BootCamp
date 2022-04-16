4af9006b2e5e73114ac85d39968bfc85f2a92c81
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2022-04-16 00:13:34 -0500

    fix

--- movementCredit-service/src/main/java/com/nttdata/movementCreditservice/serviceImpl/MovementCreditServiceImpl.java
+++ movementCredit-service/src/main/java/com/nttdata/movementCreditservice/serviceImpl/MovementCreditServiceImpl.java
@@ -1,21 +1,26 @@
 package com.nttdata.movementCreditservice.serviceImpl;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.nttdata.movementCreditservice.entity.MovementCredit;
 import com.nttdata.movementCreditservice.model.Credit;
 import com.nttdata.movementCreditservice.repository.MovementCreditRepository;
 import com.nttdata.movementCreditservice.service.MovementCreditService;
+ 
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -24,9 +29,10 @@ public class MovementCreditServiceImpl implements MovementCreditService {
 	Logger log = LoggerFactory.getLogger(MovementCreditService.class);
 	@Autowired
 	MovementCreditRepository movementCreditRepository;
-
 	@Autowired
 	RestTemplate restTemplate;
+	@Value("${api.credit-service.uri}")
+	private String creditService ;
 
 	@Override
 	public Flux<MovementCredit> findAll() {
@@ -56,14 +62,38 @@ public class MovementCreditServiceImpl implements MovementCreditService {
 
 	@Override
 	public Map<String, Object> recordsMovement(MovementCredit movementCredit) {
-		// TODO Auto-generated method stub
+		Map<String, Object> hashMap = new HashMap<String, Object>();
+		boolean isValid = true;
+		Credit credit = null;
+		if (this.findByIdProduc(movementCredit.getIdCredit()) != null) {
+			credit = this.findByIdProduc(movementCredit.getIdCredit());				 
+			/*Flux.fromIterable( (Iterable<? extends List>) this.findAll()
+			 .takeUntil(objCredit->objCredit.getIdCredit()==movementCredit.getIdCredit())
+			 .collectList()).subscribe(z->log.info(z.toString()));*/
+			 
+			
+			 
+			 
+			 //.collect(Collectors.summingInt(MovementCredit::get))	
+		} else {
+			hashMap.put("credit", "Cuenta de credito no existe.");
+			isValid = false;
+		}
+		
 		return null;
 	}
 
 	@Override
 	public Credit findByIdProduc(Long idCredit) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info(creditService + "/" + idCredit);
+		ResponseEntity<Credit> responseGet = restTemplate.exchange(creditService + "/" + idCredit, HttpMethod.GET,
+				null, new ParameterizedTypeReference<Credit>() {
+				});
+		if (responseGet.getStatusCode() == HttpStatus.OK) {
+			return responseGet.getBody();
+		} else {
+			return null;
+		}
 	}
 
 }
