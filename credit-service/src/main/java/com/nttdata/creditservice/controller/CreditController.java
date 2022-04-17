0e85a0076d11d736f6d488eff7aedd093370ff97
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2022-04-16 22:10:47 -0500

    final

--- credit-service/src/main/java/com/nttdata/creditservice/controller/CreditController.java
+++ credit-service/src/main/java/com/nttdata/creditservice/controller/CreditController.java
@@ -1,8 +1,7 @@
 package com.nttdata.creditservice.controller;
 
-import java.util.HashMap;
+ 
 import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
