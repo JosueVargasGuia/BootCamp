7eab8cd1bb2d790db7024c087ccaf755b7515e92
JostinSamuel <jostinmartinez1605@gmail.com>
2022-04-19 11:48:45 -0500

    holder-account-service

---
+++ holder-account-service/src/main/java/com/nttdata/holder/account/service/HolderAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.holder.account.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolderAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolderAccountServiceApplication.class, args);
+	}
+
+}
